70419d4db7634014bce0dd4d46da2a947b01d654
Thales Pires <thalespires45@hotmail.com>
2023-11-06 19:40:30 -0300

    Spider Tobey Update

---
+++ assets/scripts/carousel-card-3d.js
@@ -0,0 +1,16 @@
+function selectCarouselItem (selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.spider-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) -1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.spider-controller__button--active');
+    activeButtonElement.classList.remove('spider-controller__button--active');
+    selectedButtonElement.classList.add('spider-controller__button--active');
+
+}
+
